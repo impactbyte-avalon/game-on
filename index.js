e8e4839bc4031c03791a09d1c0b101444020c98c
Arian <arianmarkus77@yahoo.com>
2017-10-08 13:25:22 +0700

    Guess Number

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("GUESS PLAY");
+console.log("-----------------");
+console.log("Guess the number : from 0 to 100");
+
+function myFunction() {
+  return Math.floor(Math.random() * 100);
+}
+
+console.log(myFunction());
